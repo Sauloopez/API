3bbfaf8b6bb6e1e6b4c93d0982b409cb937b3ccc
Sauloopez <sauleta.selb@gmail.com>
2023-07-23 18:15:47 -0500

    Error arreglado
    Se tomaba el caracter: /, como una parte de la consulta

--- Router/define.php
+++ Router/define.php
@@ -4,7 +4,7 @@ $folderPath = dirname($_SERVER['SCRIPT_NAME']);
 //Esta es la request uri del usuario
 $urlPath = $_SERVER['REQUEST_URI'];
 //esta es la url sin el paquete
-$url = substr($urlPath, strlen($folderPath));
+$url = substr($urlPath, strlen($folderPath.'/'));
 
 //se define la url a asignar, ejm path/method
 define('URL', $url);
