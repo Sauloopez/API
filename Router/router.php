05eefc89d09c247af9eea70d09909e5f214c6e5f
Sauloopez <sauleta.selb@gmail.com>
2023-07-23 18:17:37 -0500

    Montaje de la nueva configuracion
    Se incluye la validacion del contenido de la consulta y además, la integración de los nuevos cambios de loos métodos de la clase Tker

--- Router/router.php
+++ Router/router.php
@@ -1,5 +1,6 @@
 <?php
 include_once(DIR.'/tk/Tker.php');
+include_once(DIR.'/tk/Validator.php');
 class Router{
     private $controller;
     private $method;
@@ -10,17 +11,23 @@ class Router{
 
     public function matchRoute(){
         
-        $params = explode('?', URL);
-        $url = explode('/', $params[0]);
+        $url = explode('.', URL);
         
-        $data= json_decode(Tker::sslurldecoder($url[1]), true);
-        define('data', $data);
-        $this->controller= $data['controller'];
-        $this->method= $data['method'];
         
-        $this->controller = $this->controller.'Controller';
+        $data= Validator::reqExT(Tker::sslurldecoder($url[0], Tker::sslurldecoder($url[1])));
         
-        require_once(__DIR__.'/controllers/'.$this->controller.'.php');
+        if(!in_array($data, Validator::$error_codes)){
+            define('data', $data);
+            $this->controller= $data['controller'];
+            $this->method= $data['method'];
+            $this->controller = $this->controller.'Controller';
+        
+            require_once(__DIR__.'/controllers/'.$this->controller.'.php');
+        }else{
+            die($data);
+        }
+        
+        
     }
 
 
