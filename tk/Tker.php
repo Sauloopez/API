a5503df5f33038358a3ca733471a2e0211c2b1f9
Sauloopez <sauleta.selb@gmail.com>
2023-07-23 16:09:15 -0500

    Cambios necesarios a la clase Tker
    Antes no se obtenía la clave IV enla codificacion, ahora las URL pueden ser dinámicas

--- tk/Tker.php
+++ tk/Tker.php
@@ -1,27 +1,51 @@
 <?php
 
-class Tker{
-    private static $secret= 'f1a7a54581b3c9d1727a5395f95579fc2e89989a372c72ac9e189e0b8e5d2599';
+class Tker
+{
+    
+    private static $secret = 'f1a7a54581b3c9d1727a5395f95579fc2e89989a372c72ac9e189e0b8e5d2599';
 
-    public static function sslurldecoder($text){
-        
+    /**
+    *Variación del campo $iv:
+    *Para desencriptar el contenido, se especifica el IV.
+    *Para desencriptar el IV, se deja en null
+    *Devuelve un string del contenido desencriptado
+    */
+    public static function sslurldecoder($text, $iv = null)
+    {
+        $iv = ($iv == null) ? 'Sondieciseiskeys': $iv;        
         $method = "camellia-128-cfb";
-        $encode= str_replace(
+        $encode = str_replace(
             ['-', '_', '*'],
             ['+', '/', '='],
             $text
         );
-    
-        return openssl_decrypt($encode, $method, Tker::$secret, null, 'Sondieciseiskeys');
+
+        return openssl_decrypt($encode, $method, Tker::$secret, null, $iv);
     }
 
-    public static function sslurlencoder($text){
-        
+
+    /**
+    *Variación del campo $with_iv:
+    *Para encriptar el contenido debe ser true, se construye un IV de manera aleatoria.
+    *Para la encriptación del IV se deja en false.
+    *Devuelve un array de dos campos, uno con el contenido cifrado y el otro con el IV
+    */
+    public static function sslurlencoder($text, $with_iv = true)
+    {
         $method = "camellia-128-cfb";
-        return str_replace(
-            ['+', '/', '='],
-            ['-', '_', '*'],
-            openssl_encrypt($text, $method, Tker::$secret, null, 'Sondieciseiskeys')
-        );
+
+        $iv = $with_iv ?
+            openssl_random_pseudo_bytes(openssl_cipher_iv_length($method)) :
+            'Sondieciseiskeys';
+
+        return [
+            'content' => str_replace(
+                ['+', '/', '='],
+                ['-', '_', '*'],
+                openssl_encrypt($text, $method, Tker::$secret, null, $iv)
+            ),
+            'iv' => $iv
+        ];
     }
 }
