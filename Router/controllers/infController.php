d72cd05501cec8dce95c906d4c26719b56c2e8b1
Sauloopez <sauleta.selb@gmail.com>
2023-07-24 19:43:59 -0500

    Controlador de informacion establecido
    Se establece el controlador de informacion para mostrar info sobre el proyecto en caso de que no exista request, <<folderPath>>

--- Router/controllers/infController.php
+++ Router/controllers/infController.php
@@ -3,6 +3,20 @@
 class infController{
 
     public static function info(){
-        echo 'info';
+        header('Content-Type: application/json');
+        echo json_encode(array(
+            'Title' => 'Welcome to myAPI RESTful',
+            'About of project' =>[
+                'Version' => '0.0.1',
+                'Birth' => 'First commit at Tue Jul 11 12:06:16 2023 -0500',
+                'About of' => 'This API receive encrypted requests via the GET method',
+            ],
+            'About of creator' => [
+                'Name' => 'Saul Lopez',
+                'Email' => 'sauleta.selb@gmail.com',
+                'GitHub' => 'https://github.com/Sauloopez',
+                'Ocupation' => 'Systems engineer student from Lejanías, Meta-Colombia'
+            ],
+        ));
     }
 }
