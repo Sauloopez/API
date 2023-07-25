f7d64bbdebe80ca2b809cc10269985dec3278a83
Sauloopez <sauleta.selb@gmail.com>
2023-07-24 19:42:51 -0500

    Sintaxis arreglada
    Se arregla la sintaxis para mostrar información en caso de que no exista una request

--- Router/router.php
+++ Router/router.php
@@ -1,40 +1,56 @@
 <?php
-include_once(DIR.'/Modules/Tker.php');
-include_once(DIR.'/Modules/Validator.php');
-class Router{
+include_once(DIR . '/Modules/Tker.php');
+include_once(DIR . '/Modules/Validator.php');
+class Router
+{
     private $controller;
     private $method;
 
-    public function __construct(){
+    public function __construct()
+    {
         $this->matchRoute();
     }
 
-    public function matchRoute(){
-        
-        $url = explode('.', $_GET['tk']);
-        
-        
-        $data= Validator::reqExT(Tker::sslurldecoder($url[0], Tker::sslurldecoder($url[1])));
-        
-        if(!in_array($data, Validator::$error_codes)){
-            define('data', $data);
-            $this->controller= $data['controller'];
-            $this->method= $data['method'];
-            $this->controller = $this->controller.'Controller';
-            
-            require_once(__DIR__.'/controllers/'.$this->controller.'.php');
+    public function matchRoute()
+    {
+        //Si se establece el tk por el método get
+        if (isset($_GET['tk'])) {
+            //Se divide el tk en el contenido y el iv, cifrados
+            $url = explode('.', $_GET['tk']);
+            //Se descifran los datos, o en dado caso, se obtiene un código de error
+            //en la validación
+            $data = Validator::reqExT(Tker::sslurldecoder($url[0], Tker::sslurldecoder($url[1])));
+            //Si no hay ningún error de la validación
+            if (!in_array($data, Validator::$error_codes)) {
+                //Se definen los datos para ser accedidos desde la API
+                define('data', $data);
+                //y además, se define el método y controlador a ejecutar
+                //el controlador se cocatena con la cadena 'Controller'
+                $this->controller = $data['controller'].'Controller';
+                $this->method = $data['method'];
+                
+            } else {
+                //en caso de tener un error se obtiene el código
+                die("Error code: $data");
+            }
         }else{
-            die("Error code: $data");
+            //En tal caso que no exista el tk, se obtiene el controlador infController
+            $this->controller ='infController';
+            $this->method = 'info';
         }
-        
-        
+        //Se requiere el controller
+        require_once(__DIR__ . '/controllers/' . $this->controller . '.php');
+
     }
 
 
-    public function run(){
+    public function run()
+    {
+        //Se crea un objeto del controller dado
         $controller = new $this->controller;
+        //Y se establece de igual manera el método
         $method = $this->method;
-        
+        //Se hace llamado a el método del controlador dado
         $controller::$method();
     }
 }
